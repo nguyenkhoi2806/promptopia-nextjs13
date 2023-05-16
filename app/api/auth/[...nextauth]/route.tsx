e040ed14899c020c216ea3169b9a59b9cff5d499
nguyenkhoi2806 <nhunguyennq@nashtechglobal.com>
2023-05-16 23:08:27 +0700

    UPDATE HANDLE AUTH WITH NEXT AUTH

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -8,37 +8,35 @@ const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET  ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
-  async session({session})  {
+  async session({ session } : any) {
     const sessionUser = await User.findOne({
-      email: session.user.email
+      email: session.user.email,
     });
     session.user.id = sessionUser._id.toString();
-    return  session;
-  }, 
-  async signIn({profile}) {
-      try {
-        await connectToDB();
-        const userExists = await User.findOne({
-          email: profile.email
+    return session;
+  },
+  async signIn({ profile }: any) {
+    try {
+      await connectToDB();
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.username.replace(" ", "").toLowerCase(),
+          image: profile.picture,
         });
-
-        if(!userExists) {
-          await User.create({
-            email: profile.email, 
-            username: profile.username.replace(" ", "").toLowerCase(),
-            image: profile.picture
-          });
-        }
-
-      } catch(error) {
-        console.log(error);
-        return false;
       }
-  }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
-
-export {handler as GET , handler as POST}
+export { handler as GET, handler as POST };
--- models/user.ts
+++ models/user.ts
@@ -3,19 +3,22 @@ import { model, models, Schema } from "mongoose";
 const userSchema = new Schema({
   email: {
     type: String,
-    unique: [true, 'Email already exists!'],
-    required: [true, 'Email is required!']
+    unique: [true, "Email already exists!"],
+    required: [true, "Email is required!"],
   },
   username: {
     type: String,
-    unique: [true, 'Username already exists!'],
-    match: [/^([0-9]+[a-zA-Z]|[a-zA-Z]+[0-9]|[a-zA-Z])+$/, 'Username is invalid']
+    unique: [true, "Username already exists!"],
+    match: [
+      /^([0-9]+[a-zA-Z]|[a-zA-Z]+[0-9]|[a-zA-Z])+$/,
+      "Username is invalid",
+    ],
   },
   image: {
-    type: String
-  }
+    type: String,
+  },
 });
 
-const User =  models.User ?? model('User', userSchema);
+const User = models.User ?? model("User", userSchema);
 
 export default User;
