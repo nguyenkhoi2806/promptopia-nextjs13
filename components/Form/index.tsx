4351a6b88faa008cd5df892d7d8f64ba9cc5dcf3
nguyenkhoi2806 <nhunguyennq@nashtechglobal.com>
2023-05-19 10:33:23 +0700

    UPDATE PRECOMMIT

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,69 +1,80 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 interface FormProps {
   type: string;
   post: any;
   setPost: (object: any) => void;
   handleSubmit: (e: any) => void;
-  submitting: boolean
+  submitting: boolean;
 }
 
 const Form = (props: FormProps) => {
   const { type, post, setPost, handleSubmit, submitting } = props;
 
   return (
-    <section
-      className="w-full max-w-full flex-start flex-col"
-    >
+    <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
-    <span  className="blue_gradient">{type} Post</span>
+        <span className="blue_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
-        {  type } and share amazing prompt with world, and let your imagination run wild with any AI-powered platform.
+        {type} and share amazing prompt with world, and let your imagination run
+        wild with any AI-powered platform.
       </p>
 
-      <form onSubmit={handleSubmit}
-      className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
-            <label>
-              <span className="font-satoshi font-semibold text-base text-gray-700">
-                  Your AI Prompt
-              </span>
-              <textarea value={post.prompt}
-              onChange={e => setPost({
-                ...post, 
-                prompt: e.target.value
-              })}
-              
-              required
-              placeholder="Write your prompt here..."
-              className="form_textarea"/>
-            </label>
-            <label>
-              <span className="font-satoshi font-semibold text-base text-gray-700">
-                  Tag 
-                  <span className="font-normal ml-2">(#product, #webdevelopment, #idea)</span>
-              </span>
-              <input value={post.tag}
-              onChange={e => setPost({
-                ...post, 
-                tag: e.target.value
-              })}
-              required
-              placeholder="#tag"
-              className="form_input"/>
-            </label>
-            <div className="flex-end -max-3 mb-5 gap-4">
-                  <Link href="/" className="text-gray-500 text-sm">
-                  Cancel
-                  </Link>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                prompt: e.target.value,
+              })
+            }
+            required
+            placeholder="Write your prompt here..."
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag
+            <span className="font-normal ml-2">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                tag: e.target.value,
+              })
+            }
+            required
+            placeholder="#tag"
+            className="form_input"
+          />
+        </label>
+        <div className="flex-end -max-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            Cancel
+          </Link>
 
-                  <button type="submit" disabled={submitting}
-                  className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
-                  {
-                    submitting ? `${type}...` : type  
-                  }
-                  </button>
-            </div>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   );
